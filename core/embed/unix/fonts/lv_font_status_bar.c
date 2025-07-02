7df57e93daf5e04e43f7127fcc33655bfb081280
ruquan.chen <ccruquan@gmail.com>
2025-07-02 16:25:04 +0800

    fix emulator fonts

--- core/embed/unix/fonts/lv_font_status_bar.c
+++ core/embed/unix/fonts/lv_font_status_bar.c
@@ -6,11 +6,7 @@
  * Opts: 
  ******************************************************************************/
 
-#ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
-#else
-#include "lvgl/lvgl.h"
-#endif
 
 #ifndef LV_FONT_STATUS_BAR
 #define LV_FONT_STATUS_BAR 1
